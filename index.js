d229bc0f50cb0a891040cbd659d067be9e2a34d1
henzy <mugunga5@gmail.com>
2021-09-08 09:16:26 +0200

    Add cdn js

---
+++ index.js
@@ -0,0 +1,51 @@
+const baseUrl = 'https://api-seo.cloudhost.cm'
+
+const apifetch = `${baseUrl}/api/seo/get-project`
+
+window.onload = async () => {
+  const scr = document.querySelector('script[id="seo-plugin"]')
+  const attr = scr.getAttribute('src')
+  const data = {
+    app_key: attr.split('?')[1],
+    domain: attr.split('-').pop()
+  }
+
+  console.log(data)
+
+  let req = {
+    method: 'POST',
+    body: JSON.stringify(data),
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json'
+    }
+  }
+  console.log(req)
+  try {
+    let get_project = await fetch(apifetch, req)
+    jsonData = await get_project.json()
+  } catch (error) {
+    console.log(req.message)
+  }
+
+  console.log(jsonData)
+  
+  if (attr.includes(data.app_key) === true) {
+    const desc = document.createElement('meta')
+    const keywords = document.createElement('meta')
+    const title = document.createElement('title')
+    const head = document.querySelector('head')
+    title.innerText = jsonData.project.meta_title
+    keywords.setAttribute('name', 'keywords')
+    keywords.setAttribute('content', jsonData.project.meta_keywords)
+    desc.setAttribute('name', 'description')
+    desc.setAttribute('content', jsonData.project.meta_description)
+    head.appendChild(desc)
+    head.appendChild(keywords)
+    head.appendChild(title)
+  } else {
+    console.log('Verify key again')
+  }
+}
+
+
